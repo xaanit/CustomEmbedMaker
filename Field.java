ff1ceb2449feef6cabcfc241601121166030c3da
xaanit <shadowjacob@yahoo.com>
2017-03-26 00:30:38 -0500

    Create Field.java

---
+++ Field.java
@@ -0,0 +1,28 @@
+package me.xaanit.customembedmaker;
+
+public class Field {
+	
+	private String title;
+	
+	private String value;
+	
+	public Field() {}
+	
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+
+}
